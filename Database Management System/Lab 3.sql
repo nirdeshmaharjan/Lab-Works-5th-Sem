5b546bc0adb296239f5e3caa7a8dc8b8f40e50e9
Nirdesh Maharjan <nirde672@gmail.com>
2025-12-27 14:33:41 +0545

    just started lab works

---
+++ Database Management System/Lab 3.sql
@@ -0,0 +1,29 @@
+USE collegedb;
+
+-- CREATE TABLE teachers (
+--   teacher_id INT PRIMARY KEY,
+--   name VARCHAR(50) NOT NULL,
+--   subject VARCHAR(50),
+--   email VARCHAR(100)
+-- );
+
+-- INSERT INTO teachers (name, subject, email) VALUES
+-- ('Mr. Sharma', 'Math', 'sharma@school.com'),
+-- ('Ms. Gupta', 'Science', 'gupta@school.com'),
+-- ('Mr. Khan', 'English', 'khan@school.com');
+
+-- SELECT * FROM teachers;
+
+-- CREATE TABLE students (
+--   student_id INT  PRIMARY KEY,
+--   name VARCHAR(50) NOT NULL,
+--   age INT,
+--   class VARCHAR(20)
+-- );
+
+-- INSERT INTO students (student_id, name, age, class) VALUES
+-- (1, 'Alice', 14, '8th Grade'),
+-- (2, 'Bob', 15, '9th Grade'),
+-- (3, 'Charlie', 13, '7th Grade');
+
+-- SELECT * FROM students;
